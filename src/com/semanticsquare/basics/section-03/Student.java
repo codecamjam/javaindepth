fe37ad02795df5bcef404e6ca783b907e1d6b312
codecamjam <camerontaghlabi@gmail.com>
2022-09-11 21:21:17 -0400

    L-027: Primitive Variables: Integers + Demo

--- src/com/semanticsquare/basics/section-03/Student.java
+++ src/com/semanticsquare/basics/section-03/Student.java
@@ -1,28 +1,34 @@
 class Student {
+    // keep in mind id and age and rank are assigned using an int literal
     int id = 1000;
-    // int id = "CAM"; NOPE!
-    // int id; // 0
-    // id = 1000 // ERROR: you cant have assignment statements at class level
+    // int id = 1000L; // incompatible types: possible lossy conversion from long to int
+    // byte age = 18;
+    byte age = 1_8; // can't do _18 or 18_
+    // byte rank = 165; // error: incompatible types: possible lossy conversion from int to byte [-128, 127]
+    short rank = 165; 
+    // int [-2^(31) - 2^(31) - 1 
+    // int phone = 2234567890; // error: integer number too large
+    // long literal
+    // int phone = 2234567890L; // error: incompatible types: possible lossy conversion from long to int
+    // long phone = 2234567890L;
+    long phone = 223_456_7890L; // can add underscores for readability (if you want)
 
-    // int id = 1000;
-    // int nextId = id + 1;
-
-    // BOTH STATIC AND INSTANCE VARIABLES ARE DECLARED AT CLASS LEVEL
-    // AND THEY GET DEFAULT VALUES 
+    //ALL 8 PRIMITIVES HAVE BOX PRIMITIVE TYPES 
+    int minValue = Integer.MIN_VALUE; //-2^(31)
+    int maxValue = Integer.MAX_VALUE; //-2^(31) - 1
+    // byte minValue = Byte.MIN_VALUE; // -128
+    // byte maxValue = Byte.MAX_VALUE; // 127
 
     void compute() {
-        // local variables don't get default values
-        // int id; this causes an error because it wasn't initialized
-
-        // this is fine
-        // int id;
-        // id = 1000;
-        //id = "Cam" NOPE, should be int
-        id = 1000; // this is fine too
         int nextId = id + 1;
 
         System.out.println("id: " + id);
         System.out.println("nextId: " + nextId);
+        System.out.println("age: " + age);
+        System.out.println("rank: " + rank);
+        System.out.println("phone: " + phone);
+        System.out.println("minValue: " + minValue);
+        System.out.println("maxValue: " + maxValue);
     }
 
     public static void main(String[] args) {
