da2064e8f3f3decfb5f1e44017d041c437ddbd8a
codecamjam <camerontaghlabi@gmail.com>
2022-09-12 19:32:34 -0400

    L-029: Primitive variables: floating-point numbers + demo

--- src/com/semanticsquare/basics/section-03/Student.java
+++ src/com/semanticsquare/basics/section-03/Student.java
@@ -3,9 +3,13 @@ class Student {
     byte age = 18;
     short rank = 165; 
     long phone = 223_456_7890L;
-    
-    byte minValue = Byte.MIN_VALUE; 
-    byte maxValue = Byte.MAX_VALUE; 
+
+    // float gpa = 3.8f; //this won't work if its 3.8, b/c then its a double literal
+    // double gpa = 3.8; // or 3.8d
+    // double gpa = 3.8e4; //38000.0
+    double gpa = 3.888888888888888888888888888889999999999 // 3.888888888888889 truncated
+    //double has precision of 15 and it gets rounded
+    // float gpa = 3.888888888888888888888888888889999999999f //3.8888888
 
     void compute() {
         int nextId = id + 1;
@@ -15,8 +19,8 @@ class Student {
         System.out.println("age: " + age);
         System.out.println("rank: " + rank);
         System.out.println("phone: " + phone);
-        System.out.println("minValue: " + minValue);
-        System.out.println("maxValue: " + maxValue);
+        System.out.println("gpa: " + gpa);
+
     }
 
     public static void main(String[] args) {
