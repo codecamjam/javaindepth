c74c78631c8b56f890de8c7e23989917de387584
codecamjam <camerontaghlabi@gmail.com>
2022-09-12 22:28:20 -0400

    L-041: 3D Arrays + Demo

--- src/com/semanticsquare/basics/section-03/Student.java
+++ src/com/semanticsquare/basics/section-03/Student.java
@@ -1,76 +1,114 @@
-class Student {
-    static int computeCount;
-    int id;
-    String name;
-    String gender;
-    int age;
-    long phone;
-    double gpa;
-    char degree;
-
-    boolean international;
-    double tuitionFees = 12000.0, internationalFees = 5000.0;
-
-
-    void compute() {
-        computeCount = computeCount + 1;
-        int nextId = id + 1;
-
-        if (international) {
-            tuitionFees = tuitionFees + internationalFees;
-        }
-
-        System.out.println("\nid: " + id);
-        System.out.println("nextId: " + nextId);
-        System.out.println("name: " + name);
-        System.out.println("age: " + age);
-        System.out.println("gender: " + gender);
-        System.out.println("phone: " + phone);
-        System.out.println("gpa: " + gpa);
-        System.out.println("degree: " + degree);
-        System.out.println("tuitionFees: " + tuitionFees);
-        System.out.println("computeCount: " + computeCount);
-
+class BasicsDemo {
+    // Adapted from http://www.ntu.edu.sg/home/ehchua/programming/java/J1a_Introduction.html
+    static void print() {
+        System.out.println("\n\nInside print ...");
+        System.out.println("Hello, world!!"); // Advance cursor to beginning of next line
+        System.out.println();                 // Print empty line
+        System.out.print("Hello, world!!");   // Cursor stayed after the printed string
+        System.out.println("Hello,");
+        System.out.print(" ");                // Print a space
+        System.out.print("world!!");
     }
 
-    public static void main(String[] args) {
-        Student student1 = new Student();
-        student1.id = 1000;
-        student1.name = "John";
-        student1.gender = "male";
-        student1.age = 18;
-        student1.phone = 223_456_7890L;
-        student1.gpa = 3.8;
-        student1.degree = 'B';
-        student1.international = false;
-        student1.compute();
-
-        Student student2 = new Student();
-        student2.id = 1001;
-        student2.name = "Raj";
-        student2.gender = "male";
-        student2.age = 21;
-        student2.phone = 223_456_9999L;
-        student2.gpa = 3.4;
-        student2.degree = 'M';
-        student2.international = true;
-        student2.compute();
-
-        Student student3 = new Student();
-        student3.id = 1002;
-        student3.name = "Anita";
-        student3.gender = "female";
-        student3.age = 20;
-        student3.phone = 223_456_8888L;
-        student3.gpa = 4.0;
-        student3.degree = 'M';
-        student3.international = true;
-        student3.compute();
-
-        student2.compute();
-        student2.compute();
-        student2.compute();
-
-        System.out.println("Student.computeCount: " + Student.computeCount);
+    static void primitives() {
+        System.out.println("\n\nInside primitives ..."); 
+        // literals demo
+        int intHex = 0x0041;
+        System.out.println("intHex: " + intHex);
+        int intBinary = 0b01000001;
+        System.out.println("intBinary: " + intBinary);
+        int intChar = 'A'; // holds decimal equivalent, which is 65
+        System.out.println("intChar: " + intChar);
+        int intUnderscore = 1_23_456;
+        System.out.println("intUnderscore: " + intUnderscore);
+        
+        char charA = 'A';
+        System.out.println("charA: " + charA);
+        char charInt = 65;
+        System.out.println("charInt: " + charInt);
+        char charUnicode1 = '\u0041'; // 4 * (16 power 1 )+ 1 * (16 power 0)
+        System.out.println("charUnicode1: " + charUnicode1);
+        char charUnicode2 = 0x41;     // 4 * (16 power 1 )+ 1 * (16 power 0)
+        System.out.println("charUnicode2: " + charUnicode2);
+        char charBinary = 0b01000001; // binary corresponding to decimal 65
+        System.out.println("charBinary: " + charBinary);	  		  	  
     }
+
+    static void typeCasting() {
+        System.out.println("\nInside typeCasting ...");
+        // Explicit casting
+        long y = 42;
+        //int x = y;
+        int x = (int)y;
+        
+        // Information loss due to out-of-range assignment
+        byte narrowdByte = (byte)123456;
+        System.out.println("narrowdByte: " + narrowdByte); 
+        
+        // Truncation
+        int iTruncated = (int)0.99;
+        System.out.println("iTruncated: " + iTruncated); 
+        
+        // Implicit cast (int to long)
+        y = x;
+        
+        // Implicit cast (char to int)
+        char cChar = 'A';
+        int iInt = cChar; 
+        System.out.println("iInt: " + iInt);
+        
+        // byte to char using an explicit cast
+        byte bByte = 65;
+        cChar = (char)bByte; // special conversion (widening from byte --> int followed by narrowing from int --> char)
+        System.out.println("cChar: " + cChar);
+    }
+
+    static void arrays() {
+        System.out.println("\nInside arrays ...");
+        int[] myArray = new int[]{9, 11, 2, 5, 4, 4, 6};
+        System.out.println("myArray.length: " + myArray.length);
+        System.out.println("myArray[1]: " + myArray[1]);
+        System.out.println("myArray[7]: " + myArray[7]);	   
+    }
+
+    static void threeDimensionalArrays() {
+        System.out.println("\nInside threeDimensionalArrays ...");
+        int[][][] unitsSold = new int[][][]{ 
+                                            { // New York
+                                                {0,0,0,0}, // Jan
+                                                {0,0,0,0}, // Feb
+                                                {0,0,0,0}, // Mar
+                                                {0,850,0,0}// Apr	
+                                            },
+                                            { // San Francisco
+                                                {0,0,0,0}, // Jan
+                                                {0,0,0,0}, // Feb
+                                                {0,0,0,0}, // Mar
+                                                {0,0,0,0}  // Apr
+                                            },
+                                            { 
+                                                {0,0,0,0},
+                                                {0,0,0,0},
+                                                {0,0,0,0},
+                                                {0,0,0,0}
+                                            },
+                                            {
+                                                {0,0,0,0},
+                                                {0,0,0,0},
+                                                {0,0,0,0},
+                                                {0,0,0,0}
+                                            }
+                                            };
+                                    
+        System.out.println("unitsSold[0][3][1]: " + unitsSold[0][3][1]);
+    }	
+
+    public static void main(String[] args) {	
+        // Language Basics 1
+        //print();				
+        //primitives();
+        //typeCasting();
+        //arrays();	
+        threeDimensionalArrays();	  
+    }       
 }
