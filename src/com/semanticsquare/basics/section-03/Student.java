d604e84137c11fee0efdfddb6f058ec4588029da
codecamjam <camerontaghlabi@gmail.com>
2022-09-12 20:07:13 -0400

    L-033: Demo: primitive variables: boolean data type

--- src/com/semanticsquare/basics/section-03/Student.java
+++ src/com/semanticsquare/basics/section-03/Student.java
@@ -3,13 +3,20 @@ class Student {
     byte age = 18;
     short rank = 165; 
     long phone = 223_456_7890L;
-
     double gpa = 3.8;
-
     char degree = 'B';
+
+    boolean international = true;
+    double tuitionFees = 12000.0;
+    double internationalFees = 5000.0;
 
     void compute() {
         int nextId = id + 1;
+
+        // if (international == true) {
+        if (international) {
+            tuitionFees = tuitionFees + internationalFees;
+        }
 
         System.out.println("id: " + id);
         System.out.println("nextId: " + nextId);
@@ -18,6 +25,7 @@ class Student {
         System.out.println("phone: " + phone);
         System.out.println("gpa: " + gpa);
         System.out.println("degree: " + degree);
+        System.out.println("tuitionFees: " + tuitionFees);
 
     }
 
