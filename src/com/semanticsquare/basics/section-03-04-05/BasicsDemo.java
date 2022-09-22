1c2d80fceb467cbe94ae5f1a970f17d9ac7c8946
codecamjam <camerontaghlabi@gmail.com>
2022-09-21 21:35:58 -0400

    L-094: String class + demo

--- src/com/semanticsquare/basics/section-03-04-05/BasicsDemo.java
+++ src/com/semanticsquare/basics/section-03-04-05/BasicsDemo.java
@@ -1,16 +1,8 @@
-// import java.util.Date;
-// import java.util.*;
-import java.util.Date;
-import java.util.ArrayList;
-
-//YOU CANNOT DO THIS:
-// import java.util.Date;
-// import java.sql.Date;
-
-
-class BasicsDemo {
-	// Adapted from http://www.ntu.edu.sg/home/ehchua/programming/java/J1a_Introduction.html
-	static void print() {
+  //package com.semanticsquare.basics;
+  
+  class BasicsDemo {
+    // Adapted from http://www.ntu.edu.sg/home/ehchua/programming/java/J1a_Introduction.html
+    static void print() {
 		System.out.println("\n\nInside print ...");
 		System.out.println("Hello, world!!"); // Advance cursor to beginning of next line
 		System.out.println();                 // Print empty line
@@ -18,8 +10,8 @@ class BasicsDemo {
 		System.out.println("Hello,");
 		System.out.print(" ");                // Print a space
 		System.out.print("world!!");
-	}
-
+    }
+	
 	static void primitives() {
 		System.out.println("\n\nInside primitives ..."); 
 		// literals demo
@@ -42,9 +34,9 @@ class BasicsDemo {
 		System.out.println("charUnicode2: " + charUnicode2);
 		char charBinary = 0b01000001; // binary corresponding to decimal 65
 		System.out.println("charBinary: " + charBinary);	  		  	  
-	}
-
-	static void typeCasting() {
+    }
+  
+    static void typeCasting() {
 		System.out.println("\nInside typeCasting ...");
 		// Explicit casting
 		long y = 42;
@@ -71,8 +63,8 @@ class BasicsDemo {
 		byte bByte = 65;
 		cChar = (char)bByte; // special conversion (widening from byte --> int followed by narrowing from int --> char)
 		System.out.println("cChar: " + cChar);
-	}
-
+    }
+	
 	static void arrays() {
 		System.out.println("\nInside arrays ...");
 		int[] myArray = new int[]{9, 11, 2, 5, 4, 4, 6};
@@ -81,46 +73,46 @@ class BasicsDemo {
 		System.out.println("myArray[7]: " + myArray[7]);	   
 	}
 
-	static void threeDimensionalArrays() {
-		System.out.println("\nInside threeDimensionalArrays ...");
-		int[][][] unitsSold = new int[][][]{ 
-											{ // New York
-												{0,0,0,0}, // Jan
-												{0,0,0,0}, // Feb
-												{0,0,0,0}, // Mar
-												{0,850,0,0}// Apr	
-											},
-											{ // San Francisco
-												{0,0,0,0}, // Jan
-												{0,0,0,0}, // Feb
-												{0,0,0,0}, // Mar
-												{0,0,0,0}  // Apr
-											},
-											{ 
-												{0,0,0,0},
-												{0,0,0,0},
-												{0,0,0,0},
-												{0,0,0,0}
-											},
-											{
-												{0,0,0,0},
-												{0,0,0,0},
-												{0,0,0,0},
-												{0,0,0,0}
-											}
-											};
-									
+    static void threeDimensionalArrays() {
+	   System.out.println("\nInside threeDimensionalArrays ...");
+	   int[][][] unitsSold = new int[][][]{ 
+										   { // New York
+											 {0,0,0,0}, // Jan
+											 {0,0,0,0}, // Feb
+											 {0,0,0,0}, // Mar
+											 {0,850,0,0}// Apr	
+										   },
+										   { // San Francisco
+											 {0,0,0,0}, // Jan
+											 {0,0,0,0}, // Feb
+											 {0,0,0,0}, // Mar
+											 {0,0,0,0}  // Apr
+										   },
+										   { 
+											 {0,0,0,0},
+											 {0,0,0,0},
+											 {0,0,0,0},
+											 {0,0,0,0}
+										   },
+										   {
+											 {0,0,0,0},
+											 {0,0,0,0},
+											 {0,0,0,0},
+											 {0,0,0,0}
+										   }
+										 };
+		                    	 
 		System.out.println("unitsSold[0][3][1]: " + unitsSold[0][3][1]);
 	}	
-
+	
 	static void varargsOverload(boolean b, int i, int j, int k){
-		System.out.println("\nInside varargsOverload without varargs ...");
+	   System.out.println("\nInside varargsOverload without varargs ...");
 	}
 	static void varargsOverload(boolean b, int... list){
-		System.out.println("\nInside varargsOverload with varargs ...");
-		System.out.println("list.length: " + list.length);
+	   System.out.println("\nInside varargsOverload with varargs ...");
+	   System.out.println("list.length: " + list.length);
 	}	
-
+	
 	static void charTypePromotion() {
 		System.out.println("\nInside charTypePromotion ...");
 		char char1 = 50; // Will be assigned corresponding UTF16 value 2
@@ -128,11 +120,11 @@ class BasicsDemo {
 		System.out.println("(73 - char1): " + (73 - char1)); // char1 gets promoted to int, i.e., decimal equivalent 50 in UTF16 is used	  
 		System.out.println("(char1 - '3'): " + (char1 - '3')); // char1 & '3' are promoted to ints	
 		System.out.println("('a' + 'b'): " + ('a' +'b')); // 'a' & 'b' are promoted to ints and the respective equivalents 97 & 98 are added
-	}
-
+    }
+	
 	static void bitwiseOperators() {
-		System.out.println("\nInside bitwiseOperators ...");
-		int x = 1;
+	    System.out.println("\nInside bitwiseOperators ...");
+	    int x = 1;
 		int y = 3;
 		
 		System.out.println("x & y: " + (x & y));
@@ -140,7 +132,7 @@ class BasicsDemo {
 		System.out.println("x ^ y: " + (x ^ y));
 		System.out.println("~x: " + (~x));
 		System.out.println("true & false: " + (true & false));
-			
+		 
 		char c1 = 'a'; // U+0061 --> 0110 0001
 		char c2 = 'b'; // U+0062 --> 0110 0010
 		System.out.println("c1 | c2: " + (c1 | c2)); // 0110 0011 --> 99 in decimal		
@@ -149,9 +141,9 @@ class BasicsDemo {
 		//double d1 = 3.14;
 		//double d2 = 5.15;
 		//System.out.println("d1 | d2: " + (d1 | d2));
-	}
-
-	static void switchExample() {
+    }
+  
+    static void switchExample() {
 		System.out.println("\nInside switchExample ...");
 		int month = 3;
 		switch (month) {
@@ -163,44 +155,117 @@ class BasicsDemo {
 					break;
 			default: System.out.println("April");
 		}
-	}
-
-	static void labeledBreak() {
-		System.out.println("\nInside labeledBreak ...");
-		int num = 0;
+    }
+  
+    static void labeledBreak() {
+	    System.out.println("\nInside labeledBreak ...");
+	    int num = 0;
 		
-		outermost: for (int i = 0; i < 10; i++) {
-			for (int j = 0; j < 10; j++) {    
-				if (i == 5 && j == 5) {
-					break outermost;
-				}
-				num++;
-			}
-		}
+	    outermost: for (int i = 0; i < 10; i++) {
+	        for (int j = 0; j < 10; j++) {    
+	            if (i == 5 && j == 5) {
+	                break outermost;
+	            }
+	            num++;
+	        }
+	    }
 		
-		System.out.println("num: " + num); // prints 55		
-	}
-
+	    System.out.println("num: " + num); // prints 55		
+    }
+	
 	static void labeledContinue() {
-		System.out.println("\nInside labeledContinue ...");
-		int num = 0;
+	    System.out.println("\nInside labeledContinue ...");
+	    int num = 0;
 		
-		outermost: for (int i = 0; i < 10; i++) {
-			for (int j = 0; j < 10; j++) {    
-				if (i == 5 && j == 5) {
-					continue outermost;
-				}
-				num++;
-			}
+	    outermost: for (int i = 0; i < 10; i++) {
+	        for (int j = 0; j < 10; j++) {    
+	            if (i == 5 && j == 5) {
+	                continue outermost;
+	            }
+	            num++;
+	        }
+	    }
+		
+	    System.out.println("num: " + num); // prints 55		
+    }
+	
+	static void stringExamples() {
+		System.out.println("\nInside stringExamples ...");
+		String s = "hello world!";
+		System.out.println("s: " + s);
+		
+		System.out.println("\ns.length(): " + s.length());
+		System.out.println("s.isEmpty(): " + s.isEmpty());
+		
+		// Comparison
+		System.out.println("\ns.equals(\"HELLO WORLD!\"): " + s.equals("HELLO WORLD!"));
+		System.out.println("s.equalsIgnoreCase(\"HELLO WORLD!\"): " + s.equalsIgnoreCase("HELLO WORLD!"));
+		//returns an int, - if char comes after, + if it precedes , and 0 = identical
+		//compares lexicographically
+		//so each char in the string has a unicode value
+		//so method compares the 2 unicode characters in the 2 strings char by char
+		//so here result is 32 becuase uppercase unicode letters come before lowercase letters
+		//so because the smaller chars in s comes after the uppercase H, so because of that
+		//it returns a positive integer
+		System.out.println("s.compareTo(\"HELLO WORLD!\"): " + s.compareTo("HELLO WORLD!")); 
+		//here we get -18 - because z(122) comes after h(104) (104 - 122 = -18)
+		System.out.println("s.compareTo(\"HELLO WORLD!\"): " + s.compareTo("zELLO WORLD!")); 
+		
+		// Searching
+		System.out.println("\ns.contains(\"HELLO WORLD!\"): " + s.contains("HELLO WORLD!"));
+		System.out.println("s.contains(\"world!\"): " + s.contains("world!"));
+		System.out.println("s.startsWith(\"HELLO WORLD!\"): " + s.startsWith("HELLO WORLD!"));
+		System.out.println("s.startsWith(\"hello world!\"): " + s.startsWith("hello world!"));
+		System.out.println("s.endsWith(\"!\"): " + s.endsWith("!"));
+		System.out.println("s.indexOf(\"lo\"): " + s.indexOf("lo"));
+		System.out.println("s.indexOf(\"o\"): " + s.indexOf('o'));
+		System.out.println("s.lastIndexOf(\"o\"): " + s.lastIndexOf('o'));
+		
+		// Examining individual characters
+		System.out.println("\ns.charAt(4): " + s.charAt(4));
+		
+		// Extracting substrings
+		System.out.println("\ns.substring(4): " + s.substring(4));
+		System.out.println("s.substring(4, 9): " + s.substring(4, 9));
+		
+		// Case conversions (Note: String is immutable. So, only a copy is returned)
+		System.out.println("\ns.toUpperCase(): " + s.toUpperCase());
+		System.out.println("s.toLowerCase(): " + s.toLowerCase());
+		
+		System.out.println("\ns.trim(): " + s.trim()); // returns a copy of string after trimming any leading & trailing white spaces
+		
+		
+		// Replace (e.g., replace comma's with white-space)
+		System.out.println("\ns.replaceAll(\"o\", \"r\"): " + s.replaceAll("o", "r"));
+		
+		// Split (e.g., split a document into words or split a line of text by tab or comma or white space)
+		System.out.println("\ns.split(\"o\"): ");
+		String[] sa = s.split("o");
+		for (String temp : sa) {
+			System.out.println(temp);
 		}
 		
-		System.out.println("num: " + num); // prints 55		
+		// Static method (includes overloaded methods)
+		System.out.println("\nString.valueOf(1.3): " + String.valueOf(1.3));
 	}
 		
 	public static void main(String[] args) {	
-		Date date;
-		ArrayList list;
-		//java.util.Date date = new java.util.Date();
-		//java.sql.Date date2;
-	}       
+		// Language Basics 1
+		//print();				
+		//primitives();
+		//typeCasting();
+		//arrays();	
+		//threeDimensionalArrays();	
+		/*varargsOverload(true, 1, 2, 3);
+		varargsOverload(true, 1, 2, 3, 4, 5, 6, 7, 8);	  
+		varargsOverload(true);*/
+		//charTypePromotion();
+		//bitwiseOperators();
+		//switchExample();
+		//labeledBreak();
+		//labeledContinue();
+		// import demo
+		// package creation demo
+		stringExamples();	  
+    }       
 }
